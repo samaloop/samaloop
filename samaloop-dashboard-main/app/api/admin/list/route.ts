c8cc05699c047281dcb6e49eb732cc1e6ffd1046
samaloop <loop.samaloop@gmail.com>
2025-10-29 10:58:10 +0700

    Tambah code revalidate = 0 di route admin api user

--- samaloop-dashboard-main/app/api/admin/list/route.ts
+++ samaloop-dashboard-main/app/api/admin/list/route.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export async function GET(
     req: NextRequest
